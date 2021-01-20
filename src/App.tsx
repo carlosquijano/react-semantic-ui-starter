7c537e0e7ec945bcb46a8a609e5414d3604a707b
CQ <carlos.quijano@gmail.com>
2021-01-19 20:28:13 -0600

    Initial semantic ui support

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import HelloWorld from "components/HelloWorld";
+import {Container} from "semantic-ui-react";
 
-const App: React.FC = () => <HelloWorld />;
+const App: React.FC = () => (
+    <Container>
+        <HelloWorld />
+    </Container>
+);
 
 export default App;
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,7 @@ import webpack, {Configuration} from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import {TsconfigPathsPlugin} from "tsconfig-paths-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const webpackConfig = (env): Configuration => ({
     entry: "./src/index.tsx",
@@ -11,7 +12,13 @@ const webpackConfig = (env): Configuration => ({
         extensions: [".ts", ".tsx", ".js"],
         //TODO waiting on https://github.com/dividab/tsconfig-paths-webpack-plugin/issues/61
         //@ts-ignore
-        plugins: [new TsconfigPathsPlugin()]
+        plugins: [new TsconfigPathsPlugin()],
+        alias: {
+            '../../theme.config$': path.join(
+                __dirname,
+                'src/custom-theme/theme.config',
+            ),
+        },
     },
     output: {
         path: path.join(__dirname, "/dist"),
@@ -26,11 +33,30 @@ const webpackConfig = (env): Configuration => ({
                     transpileOnly: true
                 },
                 exclude: /dist/
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'less-loader',
+                ],
+            },
+            { // Load fonts
+                test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+                use: 'url-loader',
+            },
+            { // Load other files, images etc
+                test: /\.(png|j?g|gif|ico)?$/,
+                use: 'url-loader',
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
+            inject: true,
             template: "./public/index.html"
         }),
         new webpack.DefinePlugin({
@@ -42,7 +68,10 @@ const webpackConfig = (env): Configuration => ({
             eslint: {
                 files: "./src/**/*.{ts,tsx,js,jsx}" // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
             }
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].[contenthash].css',
+        }),
     ]
 });
 
